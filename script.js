2e22da9694438db068023ddd21ff51d1319b9fcb
igor-ribeir0 <igor_ribeirosantos@outlook.com>
2022-10-15 14:27:30 -0300

    versão layout completo + seleção de itens

---
+++ script.js
@@ -0,0 +1,59 @@
+/*INÍCIO PRATO PRINCIPAL*/
+function prato1(){
+    document.getElementById("prato1").style.borderColor = "green";
+    document.getElementById("prato2").style.borderColor = "#FFFFFF";
+    document.getElementById("prato3").style.borderColor = "#FFFFFF";
+}
+
+function prato2(){
+    document.getElementById("prato2").style.borderColor = "green";
+    document.getElementById("prato1").style.borderColor = "#FFFFFF";
+    document.getElementById("prato3").style.borderColor = "#FFFFFF";
+}
+
+function prato3(){
+    document.getElementById("prato3").style.borderColor = "green";
+    document.getElementById("prato1").style.borderColor = "#FFFFFF";
+    document.getElementById("prato2").style.borderColor = "#FFFFFF";
+}
+/*FIM PRATO PRINCIPAL*/
+
+/*INÍCIO BEBIDAS*/
+function bebida1(){
+    document.getElementById("bebida1").style.borderColor = "green";
+    document.getElementById("bebida2").style.borderColor = "#FFFFFF";
+    document.getElementById("bebida3").style.borderColor = "#FFFFFF";
+}
+
+function bebida2(){
+    document.getElementById("bebida2").style.borderColor = "green";
+    document.getElementById("bebida1").style.borderColor = "#FFFFFF";
+    document.getElementById("bebida3").style.borderColor = "#FFFFFF";
+}
+
+function bebida3(){
+    document.getElementById("bebida3").style.borderColor = "green";
+    document.getElementById("bebida1").style.borderColor = "#FFFFFF";
+    document.getElementById("bebida2").style.borderColor = "#FFFFFF";
+}
+/*FIM BEBIDAS*/
+
+/*INÍCIO SOBREMESA*/
+function sobremesa1(){
+    document.getElementById("sobremesa1").style.borderColor = "green";
+    document.getElementById("sobremesa2").style.borderColor = "#FFFFFF";
+    document.getElementById("sobremesa3").style.borderColor = "#FFFFFF";
+}
+
+function sobremesa2(){
+    document.getElementById("sobremesa2").style.borderColor = "green";
+    document.getElementById("sobremesa1").style.borderColor = "#FFFFFF";
+    document.getElementById("sobremesa3").style.borderColor = "#FFFFFF";
+}
+
+function sobremesa3(){
+    document.getElementById("sobremesa3").style.borderColor = "green";
+    document.getElementById("sobremesa1").style.borderColor = "#FFFFFF";
+    document.getElementById("sobremesa2").style.borderColor = "#FFFFFF";
+}
+/*FIM SOBREMESA*/
